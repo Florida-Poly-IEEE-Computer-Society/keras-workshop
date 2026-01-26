47b3e4bbfe4adf63954f5bddcec4fb7961c6c05b
Nopenop <slampisehoah@gmail.com>
2026-01-26 13:13:48 -0500

    continue

---
+++ main.py
@@ -0,0 +1,12 @@
+# This workshop code is to show how you can create a forecasting model
+# using time series data
+#
+# For data
+
+
+def main():
+    print("Hello from keras-workshop!")
+
+
+if __name__ == "__main__":
+    main()
